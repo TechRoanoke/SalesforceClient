cfed034b717ae70865bc43648d520de762750de5
Mike Stall <mikestall@hotmail.com>
2016-09-05 14:10:58 -0700

    add literal helpers

---
+++ SalesforceHelpers/Literals.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Salesforce.Helpers
+{
+    public static class Literals
+    {
+        public static string ToSoql(DateTime d)
+        {
+            return d.ToString("yyyy-MM-ddThh:mm:ssZ");
+        }
+        public static string ToSoql(string x)
+        {
+            return "'" + x + "'";
+        }
+        public static string ToSoql(bool b)
+        {
+            return b.ToString().ToUpper();
+        }
+    }
+}
